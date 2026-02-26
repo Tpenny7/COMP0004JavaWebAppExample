814265c00c14c35af79bd0d4c8375a9190eaecfb
Tpenny7 <zcabhpe@ucl.ac.uk>
2026-02-26 15:49:17 +0000

    Implemented column class

--- src/main/java/uk/ac/ucl/model/Column.java
+++ src/main/java/uk/ac/ucl/model/Column.java
@@ -1,4 +1,20 @@
 package uk.ac.ucl.model;
 
+import java.util.ArrayList;
+
 public class Column {
+    String name;
+    ArrayList<String> rows;
+
+    public Column(String name){ this.name = name;}
+
+    public String getName() { return name;}
+
+    public int getSize(){ return rows.size();}
+
+    public String getRowValue(int index){ return rows.get(index);}
+
+    public void setRowValue(int index, String row){ rows.set(index,row);}
+
+    public void addRowValue(String row){ rows.add(row);}
 }
