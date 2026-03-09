34082ead86ac0eda1cbf24dcaec798feb45afac1
Tpenny7 <zcabhpe@ucl.ac.uk>
2026-03-09 15:41:46 +0000

    Added oldest and youngest person

--- src/main/java/uk/ac/ucl/model/Model.java
+++ src/main/java/uk/ac/ucl/model/Model.java
@@ -3,6 +3,7 @@ package uk.ac.ucl.model;
 import java.io.Reader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.time.LocalDate;
 import java.util.*;
 
 import uk.ac.ucl.model.io.DataLoadException;
@@ -16,7 +17,7 @@ public class Model
   DataFrame patients;
   DataLoader fileLoader = new DataLoader();
   Map<String, String> patientSummary;
-  Map<String, Integer> patientIDtoColumn;
+  Map<String, Integer> patientIDtoRow;
   ArrayList<String> columnNames;
 
   public void readFile(String fileName)
@@ -26,8 +27,38 @@ public class Model
       columnNames = patients.getColumnNames();
   }
 
-  // This also returns dummy data. The real version should use the keyword parameter to search
-  // the data and return a list of matching items.
+  public Map<String, ArrayList<String[]>> attributeNumbers(String attributeName){
+
+  }
+
+  public Map<String, ArrayList<String[]>> sameLocations(){
+    Map<String, ArrayList<String[]>> results = new HashMap<>();
+    int rowCount = patients.getRowCount();
+    //ADDRESS,CITY,STATE,ZIP
+    try {
+      Column addressCol = patients.getColumn("ADDRESS");
+      Column cityCol = patients.getColumn("CITY");
+      Column stateCol = patients.getColumn("STATE");
+      Column zipCol = patients.getColumn("ZIP");
+      Column idCol = patients.getColumn("ID");
+      for (int i = 0; i < rowCount; i++) {
+        String addr  = addressCol.getRowValue(i);
+        String city  = cityCol.getRowValue(i);
+        String state = stateCol.getRowValue(i);
+        String zip   = zipCol.getRowValue(i);
+
+        String key = addr + " | " + city + " | " + state + " | " + zip;;
+        results.putIfAbsent(key, new ArrayList<>());
+        ArrayList<String[]> list = results.get(key);
+        String id = idCol.getRowValue(i);
+        list.add(new String[]{id, patientSummary.get(id)});
+      }
+      return results;
+    }catch (ColumnNotFoundException e){
+      throw new DataLoadException("Column not found", e);
+    }
+  }
+
   public Map<String, ArrayList<String>> searchFor(String keyword) {
     Map<String, ArrayList<String>> results = new HashMap<>();
 
@@ -58,7 +89,7 @@ public class Model
 
   public void buildPatientSummary(){
     patientSummary = new HashMap<>();
-    patientIDtoColumn = new HashMap<>();
+    patientIDtoRow = new HashMap<>();
     Column firstNames = patients.getColumn("FIRST");
     Column lastNames = patients.getColumn("LAST");
     Column IDs = patients.getColumn("ID");
@@ -67,24 +98,71 @@ public class Model
       patientSummary.put(IDs.getRowValue(i),
               firstNames.getRowValue(i).split("[0-9]",2)[0] + " "
                       + lastNames.getRowValue(i).split("[0-9]",2)[0]);
-      patientIDtoColumn.put(IDs.getRowValue(i), i);
+      patientIDtoRow.put(IDs.getRowValue(i), i);
     }
   }
 
+
   public Map<String,String> getPatientSummary(){return patientSummary;}
+
+  public ArrayList<String> getAttributeNames(){return patients.getColumnNames();}
 
   public Map<String,String> getSpecificPatient(String ID){
     try {
-      int column = patientIDtoColumn.get(ID);
+      int row = patientIDtoRow.get(ID);
       Map<String, String> patient = new LinkedHashMap<>();
       for (String columnName : columnNames) {
-        patient.put(columnName, patients.getValue(columnName, column));
+        patient.put(columnName, patients.getValue(columnName, row));
       }
       return patient;
     } catch (IllegalArgumentException e){
         throw new DataLoadException("Patient with ID" + ID + "not found", e);
     } catch (ColumnNotFoundException e){
         throw new DataLoadException("Column not found", e);
+    }
+  }
+
+  public String getOldestPerson(){
+    try {
+      LocalDate oldest = LocalDate.MAX;
+      int oldestPatientRow = 0;
+      Column dobCol = patients.getColumn("BIRTHDATE");
+      Column deathCol = patients.getColumn("DEATHDATE");
+      int rowCount = patients.getRowCount();
+      for(int i = 0; i<rowCount; i++){
+        if (deathCol.getRowValue(i) == null || deathCol.getRowValue(i).isEmpty()){
+          LocalDate curr = LocalDate.parse(dobCol.getRowValue(i));
+          if(curr.isBefore(oldest)){
+            oldest = curr;
+            oldestPatientRow = i;
+          }
+        }
+      }
+      return patients.getColumn("ID").getRowValue(oldestPatientRow);
+    }catch (ColumnNotFoundException e){
+      throw new DataLoadException("Column not found", e);
+    }
+  }
+
+  public String getYoungestPerson(){
+    try {
+      LocalDate youngest = LocalDate.MIN;
+      int youngestPatientRow = 0;
+      Column dobCol = patients.getColumn("BIRTHDATE");
+      Column deathCol = patients.getColumn("DEATHDATE");
+      int rowCount = patients.getRowCount();
+      for(int i = 0; i<rowCount; i++){
+        if (deathCol.getRowValue(i) == null || deathCol.getRowValue(i).isEmpty()){
+          LocalDate curr = LocalDate.parse(dobCol.getRowValue(i));
+          if(curr.isAfter(youngest)){
+            youngest = curr;
+            youngestPatientRow = i;
+          }
+        }
+      }
+      return patients.getColumn("ID").getRowValue(youngestPatientRow);
+    }catch (ColumnNotFoundException e){
+      throw new DataLoadException("Column not found", e);
     }
   }
 
---
+++ src/main/java/uk/ac/ucl/servlets/AttributeNumbers.java
@@ -0,0 +1,23 @@
+package uk.ac.ucl.servlets;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import uk.ac.ucl.model.Model;
+import uk.ac.ucl.model.ModelFactory;
+
+import java.io.IOException;
+
+@WebServlet("/attribute")
+public class AttributeNumbers extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try{
+            request.getParameter("name");
+            Model model = ModelFactory.getModel();
+        }
+    }
+}
